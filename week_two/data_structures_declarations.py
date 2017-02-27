be95ce59b5f04369656a9870d684966ca9476985
Michael Drewitt <mikedrewitt@gmail.com>
2017-02-27 11:01:33 -0500

    adding pass so it'll run

--- week_two/data_structures_declarations.py
+++ week_two/data_structures_declarations.py
@@ -8,6 +8,7 @@ int_list = [1, 2, 3, 4, 5]
 string_int_dict = {'mike':4, 'andy':2, 'troy':5, 'alex':7}
 
 for item in string_list:
+    pass
     # item will iterate over the items in the list
 
 for key in string_int_dict:
