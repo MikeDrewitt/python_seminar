167c8ca7ba77ef7ae2569c45a77fb5b90f0a4186
Michael Drewitt <mikedrewitt@gmail.com>
2017-02-27 10:52:02 -0500

    adding some sample code for week 2 data structures

---
+++ week_two/data_structures_declarations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# how to declare a string and int list respectivly
+string_list = ['this', 'is', 'a', 'string', 'list']
+int_list = [1, 2, 3, 4, 5]
+
+# how to declare a dictionary AKA Hashmap in Java
+string_int_dict = {'mike':4, 'andy':2, 'troy':5, 'alex':7}
+
+for item in string_list:
+    # item will iterate over the items in the list
+
+for key in string_int_dict:
+    pass
+    # key holds the key
+    # we can index into a dictionary using [] to get the value
+  
